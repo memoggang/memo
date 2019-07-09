5142db24a1000e3c750e17352d9c40e9edd415f1
probreather <39427496+probreather@users.noreply.github.com>
2019-07-10 01:26:12 +0900

    gupsik

---
+++ gupsik/main.js
@@ -0,0 +1,20 @@
+const School = require('./school.js')
+
+const school = new School();
+
+school.init(School.Type.HIGH, School.Region.JEONBUK, 'P100000425');
+
+const Gupsik = async function(){
+  const meal = await school.getMeal()
+  console.log('오늘 날짜');
+  console.log(`${meal.month}월 ${meal.day}`);
+  console.log(`오늘 급식`);
+  console.log(meal.today||'오늘 급식 없음'+'\n');
+
+  console.log('이번달 급식');
+  console.log(meal);
+  console.log(' ');
+
+
+}
+Gupsik();
