6e7ffe78d05d40e7845f0554365c61a9e9b24ca2
probreather <39427496+probreather@users.noreply.github.com>
2019-07-24 11:37:05 +0900

    server upload success

--- gupsik/main.js
+++ gupsik/main.js
@@ -1,35 +1,30 @@
 const School = require('./school.js')
 const school = new School();
-var express = require('express');
-var app = express();
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT;
 
 school.init(School.Type.HIGH, School.Region.JEONBUK, 'P100000425');
 
-const Gupsik = async function(){
-  const meal = await school.getMeal()
+  const Gupsik = async function(){
+    const meal = await school.getMeal()
 
-  var accountObj = {
-    today: `${meal.month} ${meal.day}`,
-    meal1: `${meal.today}`,
-    meal2: `${meal.tomorrow}`,
-  };
-  var accountStr = JSON.stringify(accountObj);
-  module.exports = app =>{
-    app.post('/', function(req, res){
-      res.json({
-        "version": "2.0",
-        "template": {
-          "outputs": [
-            {
-              "simpleText": {
-                "text": accountStr
-              }
-            }
-          ]
-        }
+    var accountObj = {
+      today: `${meal.month} ${meal.day}`,
+      meal1: `${meal.today}`,
+      meal2: `${meal.tomorrow}`,
+    };
+    var accountStr = JSON.stringify(accountObj);
+
+    console.log(accountStr);
+      app.get('/', function(req, res){
+        res.send(accountStr);
       })
-    })
   }
-  console.log(accountStr);
-}
-Gupsik();
+  Gupsik();
+app.listen(PORT);
+
+/*
+response.writeHead(200, {'Content-Type':'text/plain; charset=utf-8'});
+response.end(JSON.stringify(accountStr));
+*/
